2d1e75cfe5856744038a838455b4a545264be7a6
yang221 <yang221@illinois.edu>
2016-04-09 15:37:45 -0500

    Added files via upload

--- app/models/User.js
+++ app/models/User.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
+var unique = require('mongoose-unique-validator');
+
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
-    username: {type: String, required: true, index:{unique: true}}, 
+    username: {type: String, required: true, unique: true}, 
     password: {type: String, required: true},
     favorids: {type: [Number], default: [] },
     interest: String
