c9f5540339243e4f33576d381c4003739e47d7b8
blaksmatic <admin@Blakss-MacBook-Pro.local>
2016-04-22 02:16:05 -0500

    done

--- public/js/app.js
+++ public/js/app.js
@@ -32,3 +32,20 @@ app.config(function ($routeProvider) {
 
 
 });
+
+
+app.filter('unique', function() {
+    return function(collection, keyname) {
+        var output = [],
+            keys = [];
+
+        angular.forEach(collection, function(item) {
+            var key = item[keyname];
+            if(keys.indexOf(key) === -1) {
+                keys.push(key);
+                output.push(item);
+            }
+        });
+        return output;
+    };
+});
