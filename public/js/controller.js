3cacae267c679ba7245ccf5ba53d72b70bdca75c
blaksmatic <admin@Blakss-MacBook-Pro.local>
2016-04-22 01:12:05 -0500

    git fix

--- public/js/controller.js
+++ public/js/controller.js
@@ -59,13 +59,13 @@ app.controller('mainController', ['$scope', '$http', '$location', 'Pagination', 
     };
 
     $scope.link_me = function () {
-        console.log("Start link");
+
 
         $http({
             method: 'GET',
             url: '/user/getRec/' + CommonData.getData().username
         }).then(function (data) {
-            //console.log(data.data);
+            console.log(data.data);
             $scope.suggest = data.data.slice(0, 99);
             $scope.pagination = Pagination.getNew(10);
             $scope.pagination.numPages = Math.ceil($scope.suggest.length / $scope.pagination.perPage);
